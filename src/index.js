8f14ebeb04cb647628b89a5e315b26d85550c0f0
Michał Grzegorczyk <grzegorczykmichal@users.noreply.github.com>
2018-06-21 21:55:55 +0200

    Added dynamic styling

--- src/index.js
+++ src/index.js
@@ -4,27 +4,70 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class Menu extends React.Component {
+  state = {
+    offsetY: 0,
+    offsetX: 0
+  };
+
   componentDidMount() {
     document.addEventListener("mousedown", ({ target }) => {
       if (this.wrapper && !this.wrapper.contains(target)) {
         this.props.trigger === target || this.props.onHide();
       }
     });
+    const listBCR = this.list.getBoundingClientRect();
+
+    const offsetY =
+      window.innerHeight - (listBCR.y + listBCR.height) < 0
+        ? -listBCR.height
+        : 0;
+    const offsetX =
+      window.innerWidth - (listBCR.x + listBCR.width) < 0 ? -listBCR.width : 0;
+
+    console.log({
+      offsetY,
+      offsetX
+    });
+
+    this.setState({
+      offsetY,
+      offsetX
+    });
   }
 
-  render() {
-    const { items } = this.props;
+  componentDidUpdate() {}
 
-    console.log(this.target.getBou)
+  render() {
+    const { items, trigger } = this.props;
+
+    const triggerBCR = trigger.getBoundingClientRect();
+
+    const dynamicStyles = {
+      top: `${triggerBCR.y + window.scrollY + this.state.offsetY}px`,
+      left: `${triggerBCR.x + window.scrollX + +this.state.offsetX}px`
+    };
 
     return (
       <div
+        style={dynamicStyles}
         className="Menu"
         ref={node => {
           this.wrapper = node;
         }}
       >
-        {!!items.length && <ul>{items.map(item => <li>{item}</li>)}</ul>}
+        {!!items.length && (
+          <ul ref={node => (this.list = node)}>
+            {items.map(item => (
+              <li
+                onClick={() => {
+                  this.props.onHide();
+                }}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
@@ -40,7 +83,10 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <div style={{ height: "20rem" }} />
+        <div style={{ height: "20rem" }} />
         <button
+          className="Button"
           ref={node => (this.trigger = node)}
           onClick={({ target }) => {
             this.setState(({ menus }) => {
